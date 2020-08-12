72aa1a9cb19e56a914b08a30e0aeafcfd7cb63ee
Shane Farmer <shane.farmer@gmail.com>
2020-08-12 10:25:39 +1000

    fix(autocomplete): add origin parameter to autocomplete queries
    When the `origin` parameter is included, the response will contain the distance in meters for each
    response. The paramter has been added to both `autocomplete` and `queryAutocomplete`.
    
    Also added `distanceMeters` to the prediction response and added test cases for decoding JSON
    responses.
    
    Fixes issue lejard-h/google_maps_webservice#74

--- lib/src/places.dart
+++ lib/src/places.dart
@@ -139,6 +139,7 @@ class GoogleMapsPlaces extends GoogleWebService {
     String input, {
     String sessionToken,
     num offset,
+    Location origin,
     Location location,
     num radius,
     String language,
@@ -150,6 +151,7 @@ class GoogleMapsPlaces extends GoogleWebService {
     final url = buildAutocompleteUrl(
       sessionToken: sessionToken,
       input: input,
+      origin: origin,
       location: location,
       offset: offset,
       radius: radius,
@@ -162,10 +164,17 @@ class GoogleMapsPlaces extends GoogleWebService {
     return _decodeAutocompleteResponse(await doGet(url));
   }
 
-  Future<PlacesAutocompleteResponse> queryAutocomplete(String input,
-      {num offset, Location location, num radius, String language}) async {
+  Future<PlacesAutocompleteResponse> queryAutocomplete(
+    String input, {
+    num offset,
+    Location origin,
+    Location location,
+    num radius,
+    String language,
+  }) async {
     final url = buildQueryAutocompleteUrl(
       input: input,
+      origin: origin,
       location: location,
       offset: offset,
       radius: radius,
@@ -287,6 +296,7 @@ class GoogleMapsPlaces extends GoogleWebService {
     String input,
     String sessionToken,
     num offset,
+    Location origin,
     Location location,
     num radius,
     String language,
@@ -298,6 +308,7 @@ class GoogleMapsPlaces extends GoogleWebService {
     final params = {
       'input': input != null ? Uri.encodeComponent(input) : null,
       'language': language,
+      'origin': origin,
       'location': location,
       'radius': radius,
       'types': types,
@@ -318,6 +329,7 @@ class GoogleMapsPlaces extends GoogleWebService {
   String buildQueryAutocompleteUrl({
     String input,
     num offset,
+    Location origin,
     Location location,
     num radius,
     String language,
@@ -325,6 +337,7 @@ class GoogleMapsPlaces extends GoogleWebService {
     final params = {
       'input': input != null ? Uri.encodeComponent(input) : null,
       'language': language,
+      'origin': origin,
       'location': location,
       'radius': radius,
       'offset': offset,
@@ -816,6 +829,7 @@ class Prediction {
   final String description;
   final String id;
   final List<Term> terms;
+  final int distanceMeters;
 
   /// JSON place_id
   final String placeId;
@@ -831,6 +845,7 @@ class Prediction {
       this.description,
       this.id,
       this.terms,
+      this.distanceMeters,
       this.placeId,
       this.reference,
       this.types,
@@ -842,6 +857,7 @@ class Prediction {
           json['description'],
           json['id'],
           json['terms']?.map((t) => Term.fromJson(t))?.toList()?.cast<Term>(),
+          json['distance_meters'],
           json['place_id'],
           json['reference'],
           json['types'] != null ? (json['types'] as List)?.cast<String>() : [],
@@ -864,6 +880,22 @@ class Term {
 
   factory Term.fromJson(Map json) =>
       json != null ? Term(json['offset'], json['value']) : null;
+
+  @override
+  String toString() {
+    return 'Term{offset: $offset, value: $value}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Term &&
+          runtimeType == other.runtimeType &&
+          offset == other.offset &&
+          value == other.value;
+
+  @override
+  int get hashCode => offset.hashCode ^ value.hashCode;
 }
 
 class MatchedSubstring {
@@ -874,6 +906,22 @@ class MatchedSubstring {
 
   factory MatchedSubstring.fromJson(Map json) =>
       json != null ? MatchedSubstring(json['offset'], json['length']) : null;
+
+  @override
+  String toString() {
+    return 'MatchedSubstring{offset: $offset, length: $length}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MatchedSubstring &&
+          runtimeType == other.runtimeType &&
+          offset == other.offset &&
+          length == other.length;
+
+  @override
+  int get hashCode => offset.hashCode ^ length.hashCode;
 }
 
 class StructuredFormatting {
