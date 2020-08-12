7b75b12236e0eadd716076993bfd0053fb61da97
Hadrien Lejard <hadrien.lejard@gmail.com>
2020-08-12 17:55:35 +0200

    analyze and format

--- example/staticmap.dart
+++ example/staticmap.dart
@@ -6,17 +6,17 @@ import 'package:google_maps_webservice/staticmap.dart';
 final apiKey = 'APIKEY';
 
 void main() {
-  StaticMap staticMap = StaticMap(
+  final staticMap = StaticMap(
     apiKey,
     markers: List.from([
       Location(23.721160, 90.394435),
-      Location(23.732322, 90.385142),]
-    ),
+      Location(23.732322, 90.385142),
+    ]),
     path: Path(
-      enc: 'svh~F`j}uOusC`bD', 
+      enc: 'svh~F`j}uOusC`bD',
       color: 'black',
     ),
-    scale: 'false'
+    scale: 'false',
   );
 
   staticMap.getUrl();
--- lib/src/staticmap.dart
+++ lib/src/staticmap.dart
@@ -1,11 +1,9 @@
-
-
 import 'package:google_maps_webservice/distance.dart';
 
 class Path {
   String color;
   String enc;
-  
+
   Path({
     this.color,
     this.enc,
@@ -15,11 +13,9 @@ class Path {
   String toString() {
     return 'color:$color|enc:$enc';
   }
-  
 }
 
 class StaticMap {
-
   final String polyEncode;
   final List<Location> markers;
   final String _apiKey;
@@ -29,29 +25,29 @@ class StaticMap {
   final String scale;
   final Path path;
   final String mapType;
-  
-  StaticMap(this._apiKey,{
-    this.polyEncode,
-    this.markers,
-    this.zoom = '', 
-    this.size = '580x267',
-    this.center = '',
-    this.scale = 'false',
-    this.path,
-    this.mapType = 'roadmap'
-  }): assert(
-    size.contains('x'),
-  );
+
+  StaticMap(this._apiKey,
+      {this.polyEncode,
+      this.markers,
+      this.zoom = '',
+      this.size = '580x267',
+      this.center = '',
+      this.scale = 'false',
+      this.path,
+      this.mapType = 'roadmap'})
+      : assert(
+          size.contains('x'),
+        );
 
   String getUrl() {
-    Map<String, dynamic> params = Map();
-    if(path != null) {
+    final params = <String, dynamic>{};
+    if (path != null) {
       params['path'] = path.toString();
     }
-    if(markers?.isNotEmpty ?? false) {
+    if (markers?.isNotEmpty ?? false) {
       params['markers'] = markers.map((l) => '|${l.lat},${l.lng}').toList();
     }
-    
+
     params.addAll({
       'key': _apiKey,
       'size': size,
@@ -62,10 +58,10 @@ class StaticMap {
     });
     print(params);
     return Uri(
-      scheme: 'https',
-      host: 'maps.googleapis.com',
-      path: 'maps/api/staticmap',
-      queryParameters: params
-    ).toString();
+            scheme: 'https',
+            host: 'maps.googleapis.com',
+            path: 'maps/api/staticmap',
+            queryParameters: params)
+        .toString();
   }
 }
